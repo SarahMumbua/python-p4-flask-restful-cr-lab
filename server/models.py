eb5cc3d0bf20007ddbd61cef94ab3af655c51764
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-07-02 00:41:00 +0300

    completed lab changes

--- server/models.py
+++ server/models.py
@@ -3,17 +3,24 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(200))
+    price = db.Column(db.Float)
 
-    def __init__(self, name):
+    def __init__(self, name, image=None, price=None):
         self.name = name
+        self.image = image
+        self.price = price
 
     def to_dict(self):
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'image': self.image,
+            'price': self.price
         }
